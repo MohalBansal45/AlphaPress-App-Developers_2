814c47e9dbbbeb0e6071aece8dd214a3121ab460
JazzB <107857696+JazzB42@users.noreply.github.com>
2023-03-31 20:32:49 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,16 @@ for (let i = 0; i < elemArr.length; i++) {
   });
 }
 
+document.addEventListener("keydown", (e) => {
+    // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+    // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+    // THIS WILL ONLY DISABLE CONTROL AND F12
+    if (e.ctrlKey || e.keyCode==123) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  });
+
 /**
  * toggle navbar & overlay when click any navbar-link
  */
